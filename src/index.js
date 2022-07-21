8b08145750a270370e6b6dbf73a33d516f21f18d
Gargi <gargideshmukh2811@gmail.com>
2022-07-21 15:09:09 +0530

    Basic Setup | NativeBase | Navigation

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './screens/login'
+import Register from './screens/register'
+
+const {Navigator, Screen} = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Navigator initialRouteName="Register">
+        <Screen name="Login" component={Login} />
+        <Screen name="Register" component={Register} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
